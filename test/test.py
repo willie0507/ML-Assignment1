aebfd2c9c3920f7fe3c83819c189da8965a39449
WILLIE-NB <willie0507@gmail.com>
2018-04-10 22:18:05 +0800

    GPU version test

--- test/test.py
+++ test/test.py
@@ -1,27 +1,17 @@
 from skimage import io, transform, color
-import numpy as np
+# import numpy as np
 from os import walk
 from os.path import join
+import tensorflow as tf
 
-'''
-im1 = io.imread('1.pgm')
-im2 = io.imread('2.pgm')
-
-x = np.array(im1)
-y = np.array(im2)
-z = abs(x/100 - y/100)
-z = z*100
-SAD = np.sum(z)
-SSD = np.sum(z**2)
-print(SAD, SSD)
-'''
-train_data = []  # (feature, label)
+train_data = []  # (filename, label)
 test_data = []
 test_data_SAD = []
 test_data_SSD = []
 train_data_array = []  # All training data in nd-array form
 test_data_array = []
 
+# Split into training data and testing data
 for root, dirs, files in walk(r"../CroppedYale"):
     count = 0
     train_data_are_set = False  # Check if training data are all prepared
@@ -35,23 +25,39 @@ for root, dirs, files in walk(r"../CroppedYale"):
         img = color.rgb2gray(img)  # Convert image to gray
         if not train_data_are_set:
             train_data.append((f, int(f[5:7])))  # Add file name and label
-            train_data_array.append(np.array(img))  # Add image in nd-array form
+            train_data_array.append(tf.constant(img))  # Add image in nd-array form
         else:
             test_data.append(f)  # Add file name
-            test_data_array.append(np.array(img))  # Add image in nd-array form
+            test_data_array.append(tf.constant(img))  # Add image in nd-array form
         count += 1
 
-for i, test_image in enumerate(test_data_array):  # Calculate SAD, SSD
-    SAD = []
-    SSD = []
-    for train_image in train_data_array:
-        z = abs(train_image/100 - test_image/100)
-        z = z*100
-        SAD.append(np.sum(z))
-        SSD.append(np.sum(z**2))
+with tf.Session() as sess:
+    test_data_array = sess.run(test_data_array)
+    train_data_array = sess.run(train_data_array)
+
+# Calculate SAD and SSD for each image
+with tf.Session() as sess:
+    for i, test_image in enumerate(test_data_array):  # Calculate SAD, SSD
+        SAD = []
+        SSD = []
+        for train_image in train_data_array:
+            z = tf.abs((tf.div(train_image, 100) - tf.div(test_image, 100)))
+            z = tf.multiply(z, 100)
+            SAD_result = tf.reduce_sum(z)
+            SSD_result = tf.reduce_sum(tf.square(z))
+            SAD_result = sess.run(SAD_result)
+            SSD_result = sess.run(SSD_result)
+            SAD.append(SAD_result)
+            SSD.append(SSD_result)
+
+        print('SAD: ', SAD)
+        print('SSD: ', SSD)
+
+'''    
     test_data_SAD.append((test_data[i], train_data[SAD.index(min(SAD))][1]))  # Find the minimum SAD
     test_data_SSD.append((test_data[i], train_data[SSD.index(min(SSD))][1]))  # Find the minimum SSD
     # test_data[i] = (test_data[i], train_data[SAD.index(min(SAD))][1])
 
 for t_data in test_data:
     print(t_data)
+'''
